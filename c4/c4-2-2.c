b22616082c0016e4c5eeabec7a0ee72ece1c416f
zhengxyz123 <3422794023@qq.com>
2024-10-29 16:02:04 +0800

    又修改了一个错误:(

--- c4/c4-2-2.c
+++ c4/c4-2-2.c
@@ -4,11 +4,9 @@ int main() {
     int year, leap;
     printf("Please input a year: ");
     scanf("%d", &year);
-    if (year % 4 == 0) {
-        leap = 0;
-        if (year % 100 != 0)
-            leap = 1;
-    } else if (year % 400 != 0)
+    if (year % 4 == 0)leap = 0;
+    else if (year % 100 != 0) leap = 1;
+    else if (year % 400 != 0)
         leap = 0;
     else
         leap = 1;
